6bb75a040a72fbbc9721298f970c36d143ec2e90
jagtapindrasen <jagtapindrasen@gmail.com>
2023-08-10 20:21:01 +0800

    Initial commit

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+
+    @Test
+    public void seleniumtest() throws InterruptedException {
+        System.out.println("Ani");
+        System.setProperty("webdriver.chrome.driver", "D:\\IntellijSampleProject\\SampleSimpleProject\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demoqa.com");
+        Thread.sleep(3000);
+        driver.quit();
+        System.out.println("Execution complete");
+
+
+    }
+}
+
